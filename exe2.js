000cd7c45d3ce0e76834d646b1c6fa7d0a62bb49
Marcelo Medeiros de Lima <marcelolima381@outlook.com>
2016-04-18 12:52:03 -0300

    Exercício 2 - Javascript - pt2
    Exercícios de mod em javascript

---
+++ exe2.js
@@ -0,0 +1,19 @@
+//exe1
+var a;
+
+function mod2(a) {
+    return a % 2 == 0;
+}
+
+a = prompt("Digite um valor");
+console.log(mod2(a));
+
+//exe2
+var num = prompt("Digite o valor do numerador");
+var den = prompt("Digite o valor do denominador");
+
+function mod(num, den) {
+    return num % den == 0;
+}
+
+console.log(mod(num,den));
